41ded57d31a321f0d13bafd488567df67a3ae5ea
jkhsjdhjs <leon.moeller@yahoo.de>
2018-09-01 02:09:51 +0000

    fix v1 reverse api

--- api/v1/img_reverse_lookup.php
+++ api/v1/img_reverse_lookup.php
@@ -7,13 +7,13 @@ header("Content-Type: application/json");
 if(@isset($_GET['image'])) {
     $image = $_GET['image'];
     $link = pg_connect(str_replace(";", " ", str_replace("pgsql:", "", $pgsql_dsn)));
-    $result = pg_query("SELECT id, file FROM reverse_lookup WHERE file LIKE '$image'");
+    $result = pg_query("SELECT post_id, file FROM reverse_lookup WHERE file LIKE '$image'");
     if(pg_num_rows($result) > 0) {
         $row = pg_fetch_object($result);
         $output = [
             "error" => "null",
-            "image" => $row->image,
-            "id" => intval($row->id)
+            "image" => $row->file,
+            "id" => intval($row->post_id)
         ];
     }
     else {
