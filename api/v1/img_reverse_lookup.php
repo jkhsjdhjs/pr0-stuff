32b5ec70c5af4facc63dc5ea734f1ecf4e05fe12
jkhsjdhjs <leon.moeller@yahoo.de>
2018-09-01 12:16:22 +0000

    shorten pgsql dsn formatting

--- api/v1/img_reverse_lookup.php
+++ api/v1/img_reverse_lookup.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 if(@isset($_GET['image'])) {
     $image = $_GET['image'];
-    $link = pg_connect(str_replace(";", " ", str_replace("pgsql:", "", $pgsql_dsn)));
+    $link = pg_connect(str_replace([";", "pgsql:"], [" "], $pgsql_dsn));
     $result = pg_query("SELECT post_id, file FROM reverse_lookup WHERE file LIKE '$image'");
     if(pg_num_rows($result) > 0) {
         $row = pg_fetch_object($result);
